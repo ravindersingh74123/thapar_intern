8260c9c691f37147ce837f331abe04b581f09f27
ravindersingh74123 <ravindersingh74123@gmail.com>
2026-03-17 14:08:15 +0530

    updating nextconfig and removing lib from root

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["pdfjs-dist", "xlsx", "tesseract.js",'duckdb', 'duckdb-async'],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Prevent any of these from being bundled for the client
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        "duckdb": false,
-        "duckdb-async": false,
-      };
-    }
-    return config;
-  },
+  turbopack: {},
+  
 };
 
 export default nextConfig;
